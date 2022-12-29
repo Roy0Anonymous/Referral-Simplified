8a4ad2909e25a51ded50122b2dd4a1ef5c839f06
Rahul Roy <64432712+Roy0Anonymous@users.noreply.github.com>
2022-12-30 00:47:30 +0530

    Initial Commit

---
+++ Referral Simplified/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Referral Simplified
+//
+//  Created by Rahul Roy on 30/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
